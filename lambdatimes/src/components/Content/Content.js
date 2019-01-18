ffd115e9dbe426364482bfae7d4f1298516b9d17
cacheUp <bradb26@gmail.com>
2019-01-18 20:27:45 +0300

    displayed tabs on screen by mapping through data, and added a click handler to the tab component.

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -23,7 +23,7 @@ export default class Content extends Component {
   }
 
   changeSelected = tab => {
-    // this function should take in the tab and update the state with the new tab.
+    this.setState({ selected: tab });
   };
 
   filterCards = () => {
@@ -50,7 +50,11 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs
+          tabs={this.state.tabs}
+          changeSelected={this.changeSelected}
+          selected={this.state.selected}
+        />
         <Cards cards={this.filterCards()} />
       </div>
     );
