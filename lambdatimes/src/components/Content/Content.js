cc0616e1be568a291a9974f1268585ea1ec2a520
cacheUp <bradb26@gmail.com>
2019-01-18 20:45:52 +0300

    added filter function for cards, mapped through the cards to display on screen, configured card component with correct props.

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -39,7 +39,14 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+
+    if (this.state.selected === "all") {
+      return this.state.cards;
+    } else {
+      let newCards = this.state.cards.slice();
+      newCards = newCards.filter(card => card.tab === this.state.selected);
+      return newCards;
+    }
   };
 
   render() {
